b342d7cec4aaf8256ae8ab35d1a3d68e98dd403d
Lucilene Cabral <lucilenecabralsenai@gmail.com>
2026-03-16 10:43:32 -0300

    Add Firebase auth and real-time multi-device sync

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+// Copie este arquivo para firebase-config.js e preencha com as credenciais do seu projeto.
+window.FIREBASE_CONFIG = {
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_PROJETO.firebaseapp.com',
+  projectId: 'SEU_PROJECT_ID',
+  storageBucket: 'SEU_PROJETO.firebasestorage.app',
+  messagingSenderId: 'SEU_SENDER_ID',
+  appId: 'SEU_APP_ID'
+};
